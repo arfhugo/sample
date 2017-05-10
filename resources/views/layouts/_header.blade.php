71f4b16ec4fa2babbbd5b9a98e6104384427d74c
arfhugo <arf.hugo@gmail.com>
2017-05-10 08:45:08 +0000

    test sshkey

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,15 @@
+<header class="navbar navbar-fixed-bottom navbar-inverse">
+    <div class="container">
+    	<div class="col-md-offset-1 col-md-10">
+    		<a href="/" id="logo">Sample App</a>
+    		<nav>
+    			<ul class="nav navbar-nav navbar-right">
+    				<li><a href="/help">幫助</a></li>
+    				<li><a href="#">登錄</a></li>
+    			</ul>
+    		</nav>
+    		
+    	</div>
+    </div>
+	
+</header>
